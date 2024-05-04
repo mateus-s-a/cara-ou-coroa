22155653b115ac56da14ebd72192788e52f9fe4f
MateusSA <99056308+mateus-s-a@users.noreply.github.com>
2024-05-03 22:34:27 -0400

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 const btn_girar = document.getElementById('btn_girar')
-let resultado = document.querySelector('#resultado')
-let res = document.createElement('h2')
+const resultado = document.querySelector('#resultado')
+const res = document.createElement('h2')
 resultado.appendChild(res)
 
 btn_girar.addEventListener('click', () => {
-    let moeda = Math.round(Math.random())
+    const moeda = Math.round(Math.random())
 
     res.classList.add('flash')
-    setTimeout(() => {
-        res.classList.remove('flash')
-    }, 500)
-    
-    if(moeda == true) {
-        res.innerHTML = 'Cara'
-    } else {
-        res.innerHTML = 'Coroa'
-    }
+    setTimeout(() => res.classList.remove('flash'), 500)
+
+    res.textContent = moeda ? 'Cara' : 'Coroa'
 })
